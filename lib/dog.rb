7b45a0426b7bc8940125237f29bbf736de383d62
wachka06 <wachka06@gmail.com>
2018-07-30 01:16:05 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 class Dog
   def name=(name)
@@ -16,3 +17,28 @@ class Dog
     @breed
   end
 end
+=======
+class Dog 
+  def initialize
+    @name = name
+    @breed = breed
+  end
+  
+   def name=(new_name)
+    @name = new_name
+  end
+  
+  def name 
+    @name
+  end
+  
+ def breed 
+   @breed = breed
+ end
+ 
+ def breed=(new_breed)
+   @breed = new_breed
+ end
+  
+end
+>>>>>>> 1abe2de59f2c5562bffefd34a7f60d198e0084cf
